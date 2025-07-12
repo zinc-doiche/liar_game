2405ac7bf573d138e86042cddda6e5bf3a99efb6
lc0709 <3dollee@gmail.com>
2025-07-13 04:16:46 +0900

    Refactor: clean export structure

--- src/loaders/event_loader.js
+++ src/loaders/event_loader.js
@@ -2,19 +2,22 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 module.exports = {
-    registerEvent(client) {
-        const eventsPath = path.join(__dirname, '../events');
-        const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+    registerEvent
+}
 
-        for (const file of eventFiles) {
-            const filePath = path.join(eventsPath, file);
-            const event = require(filePath);
 
-            if (event.once) {
-                client.once(event.name, (...args) => event.execute(...args));
-            } else {
-                client.on(event.name, (...args) => event.execute(...args));
-            }
+function registerEvent(client) {
+    const eventsPath = path.join(__dirname, '../events');
+    const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles) {
+        const filePath = path.join(eventsPath, file);
+        const event = require(filePath);
+
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
         }
     }
 }
