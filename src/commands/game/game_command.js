3b258e7215ff7d7441522db0d9750d0446fb53cc
lc0709 <3dollee@gmail.com>
2025-07-13 04:15:51 +0900

    Refactor: clean export structure

--- src/commands/game/game_command.js
+++ src/commands/game/game_command.js
@@ -2,9 +2,12 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('라이어')
-		.setDescription('라이어게임 기본 명령어'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+	.setName('라이어')
+	.setDescription('라이어게임 기본 명령어'),
+	execute
 };
+
+
+async function execute(interaction) {
+	await interaction.reply('Pong!');
+}
