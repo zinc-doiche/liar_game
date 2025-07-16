ca3d1db42485bfa66a88c09c4a6996a828066384
lc0709 <3dollee@gmail.com>
2025-07-17 03:14:19 +0900

    command test

--- src/commands/game/game_command.js
+++ src/commands/game/game_command.js
@@ -3,7 +3,15 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 	.setName('라이어')
-	.setDescription('라이어게임 기본 명령어'),
+	.setDescription('라이어게임 기본 명령어')
+	.addStringOption(option => option
+		.setName('명령')
+		.setDescription('명령을 선택합니다.')
+		.setRequired(true)
+		.addChoices(
+			{ name: '빌드', value: 'build' },
+			{ name: '시작', value: 'start' },
+		)),
 	execute
 };
 
